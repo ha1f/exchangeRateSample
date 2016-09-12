c4c041d1f5aaefcc108f276c954b1fa42d5a2209
ha1fha1f <half.ldk.hiz@gmail.com>
2016-09-12 18:07:37 +0900

    Initial Commit

---
+++ exchangeRateSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  exchangeRateSample
+//
+//  Created by 山口智生 on 2016/09/12.
+//  Copyright © 2016年 ha1f. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
